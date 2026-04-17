27ab61ce9cb39765920d02b3b9f44c32191bec95
elonnoahbiz-source <elonnoah.biz@gmail.com>
2026-04-17 22:21:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,36 +5,6 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// This makes Render think we are a website so it doesn't kill us
-app.get('/', (req, res) => {
-  res.send('KNIVES DEALER IS ONLINE');
-});
-
-// Bot Commands
-bot.start((ctx) => ctx.reply("🔪 KNIVES DEALER IS LIVE. Type /roll to play."));
-bot.command('roll', (ctx) => ctx.replyWithDice());
-
-// Start the Express server
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-
-// Start the Bot
-bot.launch().then(() => {
-  console.log('Bot is running on Telegram...');
-});
-
-// Safety stops
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
-
-const { Telegraf } = require('telegraf');
-const express = require('express');
-
-const bot = new Telegraf(process.env.BOT_TOKEN);
-const app = express();
-const PORT = process.env.PORT || 3000;
-
 // LORD CODE: A temporary "Bank" (Warning: This resets if the bot restarts)
 // We will move this to a permanent database in the next phase.
 let bank = {};
